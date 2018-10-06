3d28ade426a385f8dec0780c02b26e0859e59b3e
Jake <jjnesbitt2@gmail.com>
2018-10-06 21:55:22 +0000

    Working read access

---
+++ authListen.js
@@ -0,0 +1,59 @@
+import SpotifyWebApi from "spotify-web-api-node";
+import express from 'express';
+import {
+    client_id,
+    client_secret,
+    redirect_uri,
+} from "./credentials.json";
+
+const scopes = 'user-read-playback-state user-read-currently-playing';
+const spotifyApi = new SpotifyWebApi({
+    clientId: client_id,
+    clientSecret: client_secret,
+    redirectUri: redirect_uri,
+});
+const authorizationURL = spotifyApi.createAuthorizeURL([scopes]);
+
+const app = express();
+app.get('/sam-spotify', (req, res) => {
+    res.redirect(authorizationURL);
+});
+
+app.get('/sam-spotify/callback', (req, res) => {
+
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+
+    res.send("Hey thanks").end();
+
+    const code = req.query.code || null;
+    console.log(code);
+
+    spotifyApi.authorizationCodeGrant(code).then(
+        (data) => {
+            console.log('The token expires in ' + data.body['expires_in']);
+            console.log('The access token is ' + data.body['access_token']);
+            console.log('The refresh token is ' + data.body['refresh_token']);
+
+            // Set the access token on the API object to use it in later calls
+            spotifyApi.setAccessToken(data.body.access_token);
+            spotifyApi.setRefreshToken(data.body.refresh_token);
+
+            spotifyApi.getMyCurrentPlaybackState({}).then(
+                data => {
+                    // Output items
+                    console.log("Now Playing: ", data.body);
+                },
+                err => {
+                    console.log("Something went wrong!", err);
+                }
+            );
+        },
+        (err) => {
+            console.log('Something went wrong!', err);
+        }
+    );
+});
+
+app.listen(8888);
+console.log('Listening on 8888');
--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-const { client_id, client_secret } = require('./credentials.js');
+import SpotifyWebApi from "spotify-web-api-node";
+import _ from 'lodash';
+import {
+    client_id,
+    client_secret,
+    redirect_uri,
+    access_token,
+    refresh_token,
+} from "./credentials.json";
 
-// credentials are optional
 const spotifyApi = new SpotifyWebApi({
     clientId: client_id,
     clientSecret: client_secret,
-    redirectUri: 'http://www.example.com/callback',
+    redirectUri: redirect_uri,
 });
+spotifyApi.setAccessToken(access_token);
+spotifyApi.setRefreshToken(refresh_token);
 
-spotifyApi.getMyCurrentPlaybackState({})
-    .then(function (data) {
-        // Output items
-        console.log("Now Playing: ", data.body);
-    }, function (err) {
-        console.log('Something went wrong!', err);
-    });
+const getPlayback = () => {
+    spotifyApi.getMyCurrentPlaybackState({}).then(
+        data => {
+            // Output items
+            console.log(data);
+            if (_.isEmpty(data.body)){
+                console.log("Not playing anything!");
+            }
+            else{
+                console.log(data.body.item);
+                console.log(`Now Playing: ${data.body.item.name} by ${data.body.item.artists[0].name}`);
+                console.log("ID: ", data.body.item.id);
+            }
+        },
+        err => {
+            console.log("Something went wrong!", err);
+            refreshAccessToken();
+        }
+    );
+};
+
+const refreshAccessToken = () => {
+    spotifyApi.refreshAccessToken().then(
+        (data) => {
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body.access_token);
+            console.log(data.body.access_token);
+
+            console.log('The access token has been refreshed!');
+        },
+        (err) => {
+            console.log('Could not refresh access token', err);
+        }
+    );
+};
+
+getPlayback();
